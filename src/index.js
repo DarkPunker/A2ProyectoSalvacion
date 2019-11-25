31c467e591a89bbb33e9c84ca709256b4eeab168
DarkPunker <43015325+DarkPunker@users.noreply.github.com>
2019-11-25 02:39:18 -0500

    mvc con helpers

--- src/index.js
+++ src/index.js
@@ -17,13 +17,23 @@ require('./lib/passport');
 //settings
 app.set('port', process.env.PORT || 4000);
 app.set('views', path.join(__dirname, 'views'));
-app.engine('.hbs', exphbs({
+
+const hbs = exphbs.create({
     defaultLayout: 'main',
     layoutsDir: path.join(app.get('views'), 'layouts'),
     partialsDir: path.join(app.get('views'), 'partials'),
     extname: '.hbs',
     helpers: require('./lib/handlebars')
-}));
+});
+app.engine('.hbs', hbs.engine);
+
+/* app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('views'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars')
+})); */
 app.set('view engine', '.hbs');
 
 //middlewares
