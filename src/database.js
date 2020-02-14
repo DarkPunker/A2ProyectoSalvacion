3e411bf3ec613d574d9fecdcadf342d738809613
DarkPunker <43015325+DarkPunker@users.noreply.github.com>
2020-02-14 09:41:05 -0500

    nuevo intento

--- src/database.js
+++ src/database.js
@@ -2,32 +2,34 @@ const mongoose = require('mongoose');
 const mysql = require('mysql');
 const { promisify } = require('util');
 
-const {database} = require('./keys');
+const { database } = require('./keys');
 
 const pool = mysql.createPool(database);
 
-mongoose.connect("mongodb+srv://admin:1234@softwareeducativojson-tzliv.mongodb.net/test?retryWrites=true&w=majority",{
+mongoose.connect('mongodb+srv://admin:1234@softwareeducativojson-tzliv.mongodb.net/test?retryWrites=true&w=majority', {
+    user: "admin",
+    pass: "1234",
     useCreateIndex: true,
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false
 })
-.then(db => console.log('DB Json is connected'))
-.catch(err => console.error(err));
+    .then(db => console.log('DB Json is connected'))
+    .catch(err => console.error(err));
 
-pool.getConnection((err, connection)=>{
-    if (err){
-        if (err.code === 'PROTOCOL_CONNECTION_LOST'){
+pool.getConnection((err, connection) => {
+    if (err) {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
             console.error('DATABASE CONNECTION WAS CLOSED');
         }
-        if (err.code === 'ER_CON_COUNT_ERROR'){
+        if (err.code === 'ER_CON_COUNT_ERROR') {
             console.error('DATABASE HAS TO MANY CONNECTIONS');
         }
-        if (err.code === 'ECONNREFUSED'){
+        if (err.code === 'ECONNREFUSED') {
             console.error('DATABASE CONNECTION WAS REFUSED');
         }
     }
-    if (connection)connection.release();
+    if (connection) connection.release();
     console.log('DB SQL is Connected');
     return;
 });
